72376068c697c2bc308c3e5d7b1f53eda2695d2c
Gautam Kumar <kumargautamsp@gmail.com>
2024-08-30 03:27:13 +0530

    created user authentication and fetched food data on frontend from backend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from 'axios';
+
 
 export const StoreContext = createContext(null);
 
@@ -7,6 +8,9 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState ({})
+    const url = "http://localhost:4000"
+    const [token,setToken] = useState("");
+    const [food_list,setFoodList] = useState([])
 
   const addToCart = (itemId) =>{
     if(!cartItems[itemId]) {
@@ -35,13 +39,32 @@ const getTotalCartAmount = () =>{
         return totalAmount;
 }
 
+const fetchFoodList = async () =>{
+    const response = await axios.get(url+"/api/food/list");
+    setFoodList(response.data.data)
+}
+
+
+useEffect(()=>{
+    async function loadData() {
+        await fetchFoodList();
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"));
+        }
+    }
+    loadData();
+},[])
+
     const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
     }
     return (
         <StoreContext.Provider value={contextValue}>
