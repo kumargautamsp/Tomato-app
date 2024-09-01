15630ef13d1ab20bd9240dcc323e626e55a3a2bc
Gautam Kumar <126412810+kumargautamsp@users.noreply.github.com>
2024-09-02 01:18:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/placeorder/PlaceOrder';
+import PlaceOrder from './pages/Placeorder/PlaceOrder';
 import Footer from './components/Footer/Footer'
 import { useState } from 'react'
 import LoginPopup from './components/LoginPopup/LoginPopup'
