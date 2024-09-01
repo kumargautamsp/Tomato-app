5e0ffbd9cd8b784a5072dc60ee89c455b922c817
Gautam Kumar <126412810+kumargautamsp@users.noreply.github.com>
2024-09-01 20:55:07 +0530

    Update placeOrder.jsx

--- frontend/src/pages/placeOrder/placeOrder.jsx
+++ frontend/src/pages/placeOrder/placeOrder.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
-import './PlaceOrder.css'
+import './placeorder.css';
+
 import {StoreContext} from '../../context/StoreContext'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
